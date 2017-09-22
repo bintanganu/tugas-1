ba656165714779c7220f7a1323b05715430a672b
bintanganu <32098094+bintanganu@users.noreply.github.com>
2017-09-22 10:11:42 +0700

    bintang1
    tugas python

---
+++ tugas-1.py
@@ -0,0 +1,9 @@
+nama = "bintang anugrah"
+nim = "04217020"
+umur = 18
+print ("namaku :" + nama)
+print ("nimku :" + nim)
+print ("umur")
+print (umur)
+print ("umur 5 tahun kedepan")
+print (umur +5)
